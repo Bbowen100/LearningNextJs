64171e528c069c2fec1caa02195429c82acf3e4f
Bbowen100 <brandonklbowen@gmail.com>
2024-11-03 19:08:43 -0500

    nextjs course info added

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,11 +15,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 10 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -30,6 +30,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // console.log('Fetching invoices data...');
+    // await new Promise((resolve) => setTimeout(resolve, 6000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -50,6 +52,9 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+    // console.log('Fetching card data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
@@ -157,7 +162,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log("Invoice found", invoice)
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
